9016a2bdf73700727928c1b5aa1fc8c5f9671c4d
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-10-03 14:17:33 +0600

    updated dashboard redirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,7 +21,7 @@ const Login: NextPage = () => {
   const router = useRouter()
   const [submitting, setSubmitting] = useState(false)
 
-  const getRedirect = () => '/'
+  const getRedirect = () => '/appointments'
 
   const {
     register,
