5448b6ca43476d4e4ea21c53b8982a2dd8f28ecb
Md Sabbirul Latif Shuvo <sabbirul.latif@bjitgroup.com>
2023-10-03 13:25:59 +0600

    updated headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,33 @@ const redirectIfAuthenticated: Middleware = (request) => {
 }
 
 const authenticated: Middleware = (request) => {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'unsafe-eval';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+  requestHeaders.set('X-Frame-Options', 'SAMEORIGIN')
+  requestHeaders.set(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains;preload'
+  )
+  requestHeaders.set(
+    'Content-Security-Policy',
+    cspHeader.replace(/\s{2,}/g, ' ').trim()
+  )
+
   const authSession = request.cookies.get('auth')?.value
 
   if (!authSession) {
@@ -23,13 +50,54 @@ const authenticated: Middleware = (request) => {
       name: 'redirect',
       value: request.url,
     })
+    response.headers.set(
+      'Content-Security-Policy',
+      cspHeader.replace(/\s{2,}/g, ' ').trim()
+    )
+    response.headers.set('X-Frame-Options', 'SAMEORIGIN')
+    response.headers.set(
+      'Strict-Transport-Security',
+      'max-age=31536000; includeSubDomains;preload'
+    )
     return response
   }
 
-  return NextResponse.next()
+  return NextResponse.next({
+    headers: requestHeaders,
+    request: {
+      headers: requestHeaders,
+    },
+  })
 }
 
 export default function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'unsafe-eval';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+  requestHeaders.set('X-Frame-Options', 'SAMEORIGIN')
+  requestHeaders.set(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains;preload'
+  )
+  requestHeaders.set(
+    'Content-Security-Policy',
+    cspHeader.replace(/\s{2,}/g, ' ').trim()
+  )
+
   // Uncomment if you want to redirect if authenticated.
   // if ([
   //   '/login',
@@ -38,13 +106,22 @@ export default function middleware(request: NextRequest) {
   //   return redirectIfAuthenticated(request)
   // }
 
-  if ([
-    '/',
-    '/pokemons',
-    '/pokemons/client',
-  ].includes(request.nextUrl.pathname)) {
+  if (
+    ['/', '/pokemons', '/appointments', '/pokemons/client'].includes(
+      request.nextUrl.pathname
+    )
+  ) {
     return authenticated(request)
   }
 
-  return NextResponse.next()
+  if (request.nextUrl.pathname.match(/\/appointments\/\d+/)) {
+    return authenticated(request)
+  }
+
+  return NextResponse.next({
+    headers: requestHeaders,
+    request: {
+      headers: requestHeaders,
+    },
+  })
 }
